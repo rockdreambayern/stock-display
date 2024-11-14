ef7085b8e48d3846ad863209caa694282739cbb4
rockdreambayern <bayernro@qq.com>
2024-11-14 14:16:18 +0800

    init

---
+++ currencyrate/currency_exchange_rate.py
@@ -0,0 +1,42 @@
+# importing required libraries
+import requests
+
+
+# Function to get real time currency exchange
+def RealTimeCurrencyExchangeRate(from_currency, to_currency, api_key):
+
+    # main_url variable store complete url
+    main_url = 'https://v6.exchangerate-api.com/v6/' + api_key + "/pair/" + from_currency + "/" + to_currency
+
+    # get method of requests module
+
+    # return response object
+    req_ob = requests.get(main_url)
+
+    # json method return json format
+    # data into python dictionary data type.
+
+    # result contains list of nested dictionaries
+    result = req_ob.json()
+
+    print(" Result before parsing the json data :\n", result)
+
+    print("\n After parsing : \n Realtime Currency Exchange Rate for",
+          result["base_code"], "TO",
+          result["target_code"], "is",
+          result["conversion_rate"], to_currency)
+
+    return result["conversion_rate"]
+
+
+# Driver code
+if __name__ == "__main__":
+    # currency code
+    from_currency = "HKD"
+    to_currency = "CNY"
+
+    # enter your api key here
+    api_key = "YOUR API KEY"
+
+    # function calling
+    RealTimeCurrencyExchangeRate(from_currency, to_currency, api_key)
---
+++ stock/stock_price.py
@@ -0,0 +1,17 @@
+import math
+
+import akshare as ak
+import currencyrate.currency_exchange_rate as currency_exchange_rate
+
+
+def query_stock_price(currency_code, currency_rate_api_key):
+    stock_hk_spot_df = ak.stock_hk_main_board_spot_em()
+    target_stock = stock_hk_spot_df[stock_hk_spot_df["代码"] == "03690"]
+    latest_price = target_stock["最新价"].values[0]
+
+
+    current_exchange_rate = currency_exchange_rate.RealTimeCurrencyExchangeRate("HKD", currency_code,
+                                                                                currency_rate_api_key)
+    result = math.floor(latest_price * current_exchange_rate * 100) / 100
+
+    print(f"实时价格: {latest_price} 港币 {result} 元")
