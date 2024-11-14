21de4ffc9c4f08db85540a506603f2540dfb3312
rockdreambayern <bayernro@qq.com>
2024-11-14 17:17:29 +0800

    add html page to display result

--- api/stock_controller.py
+++ api/stock_controller.py
@@ -1,4 +1,4 @@
-from flask import json, Response, Blueprint
+from flask import json, Response, Blueprint, render_template
 import stock.stock_price as stock
 
 stock_controller = Blueprint('stock_controller', __name__)
@@ -7,9 +7,13 @@ stock_controller = Blueprint('stock_controller', __name__)
 # 获取所有数据
 @stock_controller.route('/stock', methods=['GET'])
 def get_data():
-    data = stock.query_stock_price("CNY")
+    data = stock.query_multiple_stock_price("CNY")
     # 使用 json.dumps 进行手动编码
     json_result = json.dumps(data, ensure_ascii=False)
 
     # 返回响应
     return Response(json_result, content_type='application/json; charset=utf-8')
+
+@stock_controller.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
--- stock/stock_price.py
+++ stock/stock_price.py
@@ -4,9 +4,36 @@ import akshare as ak
 import currencyrate.currency_exchange_rate as currency_exchange_rate
 
 
+def query_multiple_stock_price(currency_code):
+    stock_hk_spot_df = ak.stock_hk_main_board_spot_em()
+    target_stocks = stock_hk_spot_df[(stock_hk_spot_df["代码"] == "03690") | (stock_hk_spot_df["代码"] == "00700")]
+
+    current_exchange_rate = currency_exchange_rate.RealTimeCurrencyExchangeRate("HKD", currency_code)
+
+    data_list = []
+
+    for index, target_stock in target_stocks.iterrows():
+        latest_price = target_stock["最新价"]
+        code = target_stock["代码"]
+        name = target_stock["名称"]
+
+        result = math.floor(latest_price * current_exchange_rate * 100) / 100
+
+        data = {
+            "name": name,
+            "code": code,
+            "price": latest_price,
+            "price_cny": result
+        }
+
+        data_list.append(data)
+
+    return data_list
+
+
 def query_stock_price(currency_code):
     stock_hk_spot_df = ak.stock_hk_main_board_spot_em()
-    target_stock = stock_hk_spot_df[stock_hk_spot_df["代码"] == "03690"]
+    target_stock = stock_hk_spot_df[stock_hk_spot_df["代码"] == "03690" | ""]
     latest_price = target_stock["最新价"].values[0]
     code = target_stock["代码"].values[0]
     name = target_stock["名称"].values[0]
