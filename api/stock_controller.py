b795dca26adda6fb89a12b4dfe34855b5f73ec52
rockdreambayern <bayernro@qq.com>
2024-11-14 23:15:51 +0800

    display stock latest prices

--- api/stock_controller.py
+++ api/stock_controller.py
@@ -14,6 +14,17 @@ def get_data():
     # 返回响应
     return Response(json_result, content_type='application/json; charset=utf-8')
 
+@stock_controller.route('/stock_latest/<string:code>', methods=['GET'])
+def get_lastest_data(code):
+    data = stock.query_stock_history_prices(code)
+
+    # 使用 json.dumps 进行手动编码
+    json_result = json.dumps(data, ensure_ascii=False)
+
+    # 返回响应
+    return Response(json_result, content_type='application/json; charset=utf-8')
+
+
 @stock_controller.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
--- stock/stock_price.py
+++ stock/stock_price.py
@@ -1,4 +1,5 @@
 import math
+from datetime import datetime, timedelta
 
 import akshare as ak
 import currencyrate.currency_exchange_rate as currency_exchange_rate
@@ -33,7 +34,7 @@ def query_multiple_stock_price(currency_code):
 
 def query_stock_price(currency_code):
     stock_hk_spot_df = ak.stock_hk_main_board_spot_em()
-    target_stock = stock_hk_spot_df[stock_hk_spot_df["代码"] == "03690" | ""]
+    target_stock = stock_hk_spot_df[stock_hk_spot_df["代码"] == "03690"]
     latest_price = target_stock["最新价"].values[0]
     code = target_stock["代码"].values[0]
     name = target_stock["名称"].values[0]
@@ -53,3 +54,40 @@ def query_stock_price(currency_code):
     }
 
     return data
+
+
+def query_stock_history_prices(code):
+    # 获取当前日期
+    end_date = datetime.now()
+
+    # 计算30天前的日期
+    start_date = end_date - timedelta(days=30)
+
+    # 将日期格式化为 'yyyymmdd'
+    end_date_str = end_date.strftime('%Y%m%d')
+    start_date_str = start_date.strftime('%Y%m%d')
+
+    stock_hk_spot_em_df = ak.stock_hk_hist(symbol=code, period="daily", start_date=start_date_str,
+                                           end_date=end_date_str,
+                                           adjust="")
+
+    data_list = []
+
+    # 检查是否找到了匹配的记录
+    if stock_hk_spot_em_df.empty:
+        return data_list
+
+    for index, stock in stock_hk_spot_em_df.iterrows():
+        price = stock["收盘"]
+        date = stock['日期'].strftime("%Y-%m-%d")
+
+        data = {
+            "name": code,
+            "code": code,
+            "price": price,
+            "date": date
+        }
+
+        data_list.append(data)
+
+    return data_list
